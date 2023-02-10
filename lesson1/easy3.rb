8e545d920003b40752930c8cc28bcb5cc313b24d
babayaga24 <kumarabhishek2407@gmail.com>
2023-02-10 11:31:19 +0530

    Add easy 2 and easy 3 exercises

---
+++ lesson1/easy3.rb
@@ -0,0 +1,40 @@
+# arr = []
+# puts "Enter the 1st number:"
+# num1 = gets.chomp.to_i
+# arr.push(num1)
+
+# puts "Enter the 2nd number:"
+# num2 = gets.chomp.to_i
+# arr.push(num2)
+
+# puts "Enter the 3rd number:"
+# num3 = gets.chomp.to_i
+# arr.push(num3)
+
+# puts "Enter the 4th number:"
+# num4 = gets.chomp.to_i
+# arr.push(num4)
+
+# puts "Enter the 5th number:"
+# num5 = gets.chomp.to_i
+# arr.push(num5)
+
+# puts "Enter the last number:"
+# num6 = gets.chomp.to_i
+
+# arr.include?(num6) ? (puts "The number #{num6} appears in #{arr}.") : ((puts "The number #{num6} does not appear in #{arr}."))
+
+
+# puts "Enter the first number:"
+# num1 = gets.chomp.to_i
+
+# puts "Enter the second number:"
+# num2 = gets.chomp.to_i
+
+# puts "#{num1} + #{num2} = #{num1 + num2}"
+# puts "#{num1} - #{num2} = #{num1 - num2}"
+# puts "#{num1} * #{num2} = #{num1 * num2}"
+# puts "#{num1} / #{num2} = #{num1 / num2}"
+# puts "#{num1} % #{num2} = #{num1 % num2}"
+# puts "#{num1} ** #{num2} = #{num1 ** num2}"
+
